8cc66b7864add21ec6b1a8866165ea13c6c3d844
alexis <alexisgiubi320@gmail.com>
2021-07-28 14:24:25 -0400

    primer comentario

---
+++ noticias/noticias/urls.py
@@ -0,0 +1,44 @@
+"""noticias URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.db import router
+from rest_framework import serializers, viewsets, routers, mixins
+from django.contrib import admin
+from django.urls import path, include
+
+from app_noticias.models import Noticia
+
+
+
+class NoticiaSerializador(serializers.ModelSerializer):
+    class Meta:
+        model = Noticia
+        fields = ["title", "body"]
+        
+
+class ListaDeNoticiasView(viewsets.ModelViewSet):
+    queryset = Noticia.objects.all()
+    serializer_class = NoticiaSerializador
+
+router = routers.DefaultRouter()
+router.register(r"noticias",ListaDeNoticiasView,"noticias")
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
+]
